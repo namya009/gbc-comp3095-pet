d7c3628451a7e43bae57b398fe47dbe0d3b4748d
namya009 <namyapatel02@gmail.com>
2021-10-12 01:30:57 -0400

    Comp3095 Lab 3 - POJO Models.Closes #2

---
+++ gbc-comp3095-pet/src/main/java/ca/gbc/comp3095/gbccomp3095pet/model/Pet.java
@@ -0,0 +1,9 @@
+package ca.gbc.comp3095.gbccomp3095pet.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthdate;
+}
